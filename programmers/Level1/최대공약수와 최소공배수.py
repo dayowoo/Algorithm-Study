c6ada58a82063249f1c4c524ab0fba497d6ee262
kkbo5222 <kkbo5222@gmail.com>
2022-04-19 12:52:16 +0900

    짝수와 홀수

---
+++ programmers/Level1/최대공약수와 최소공배수.py
@@ -0,0 +1,15 @@
+'''
+import math
+math.lcn(4,6)   # 최소공배수
+d = 1
+while d%n!=0 pr d%m!=0:
+    d+=1
+math.gcd(4,6)   # 최대공약수
+'''
+import math
+
+def solution(n, m):
+    answer = []
+    return answer
+
+print(solution(3,12))
