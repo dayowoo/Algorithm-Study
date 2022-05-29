ae7e098ed6e557899d1f4afe67feaff7eeb77782
kkbo5222 <kkbo5222@gmail.com>
2022-05-29 20:05:36 +0900

    이상한 문자 만들기

---
+++ programmers/Level1/이상한 문자 만들기.py
@@ -0,0 +1,21 @@
+'''
+1. 공백을 기준으로 slice후 리스트 저장: str.slice()
+2. list[i] %2 == 0 : upper
+3. list[i] %2 != 0 : lower
+'''
+
+def solution(s):
+    answer = ''
+    new_list = s.split()
+    for i in new_list:
+        for j in range(len(i)):
+            if j%2==0:
+                answer += i[j].upper()
+            else:
+                answer += i[j].lower()
+        answer += ' '
+    return answer[0:-1]
+    
+    
+
+print(solution("try hello world"))
--- programmers/Level1/자릿수 더하기.py
+++ programmers/Level1/자릿수 더하기.py
@@ -16,10 +16,20 @@ print(sum_digit(1))
 
 
 
-# 다른사람 풀이
+# 다른사람 풀이 - 재귀구조: a함수 내에서 a함수 호출
 def sum_digit2(number):
+    # 한자리수의 경우 그냥 출력
     if number < 10:
         return number
+    # 나머지 + 몫
     return (number%10) + sum_digit(number//10)
 
 print(sum_digit2(123))
+
+
+def sum_digit3(number):
+    return sum(map(int,str(number)))
+
+
+def sum_digit4(number):
+    return sum([int(i) for i in str(number)])
