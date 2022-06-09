71b1dc0d254d5f13338e89cdbfcf47ad60bbea69
kkbo5222 <kkbo5222@gmail.com>
2022-06-09 14:52:44 +0900

    3-3 숫자 카드 게임

---
+++ 이것이 코딩테스트다/3-3 숫자 카드 게임.py
@@ -0,0 +1,18 @@
+'''
+각 행마다 가장 작은 수를 찾은 뒤에 그 수중에서 가장 큰 수를 찾기.
+'''
+
+def solution(data):
+    result = 0
+    print(data)
+    for i in range(len(data)):
+        min_value = min(data[i])
+        result = max(result, min_value)
+    return result
+
+n, m = map(int, input().split())
+data = []
+for i in range(n):
+    data.append(list(map(int, input().split())))
+
+print(solution(data))
