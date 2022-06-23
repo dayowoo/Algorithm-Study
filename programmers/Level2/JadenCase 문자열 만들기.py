b008d28472fdd6d885b59e88b99ab97cd40bbde2
kkbo5222 <kkbo5222@gmail.com>
2022-06-24 00:10:32 +0900

    JadenCase 문자열 만들기

--- programmers/Level2/JadenCase 문자열 만들기.py
+++ programmers/Level2/JadenCase 문자열 만들기.py
@@ -27,3 +27,26 @@ def solution(s):
     return answer
 
 print(solution("for the last week"))
+
+
+def solution(s):
+    answer = ''
+    s=s.split(' ')
+    for i in range(len(s)):
+        # capitalize 내장함수를 사용하면 첫 문자가 알파벳일 경우 대문자로 만들고
+        # 두번째 문자부터는 자동으로 소문자로 만든다
+        # 첫 문자가 알파벳이 아니면 그대로 리턴한다
+        s[i]=s[i].capitalize()
+    answer=' '.join(s)
+    return answer
+
+
+# 다른 풀이
+'''
+- title: 문자열을 단어 앞만 대문자로 바꿀 수 있음
+'''
+def Jaden_Case(s):
+    # 함수를 완성하세요
+    return s.title()
+
+print(Jaden_Case("3people unFollowed me for the last week"))
