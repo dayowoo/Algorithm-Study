41f4eb155a5435c67e541bd090786ed385b75e49
kkbo5222 <kkbo5222@gmail.com>
2022-06-24 00:07:27 +0900

    JadenCase 문자열 만들기

---
+++ programmers/Level2/JadenCase 문자열 만들기.py
@@ -0,0 +1,28 @@
+'''
+1. 공백 기준으로 리스트 저장
+2. i[0].upper() -> 대문자 변환
+3. 
+'''
+
+'''
+def solution(s):
+    answer = list(map(str, s.split()))
+    new_list = []
+    new = []
+    for i in answer:
+        new_list.append(i.replace(i[0], i[0].upper()))
+    for i in new_list:
+        new.append(i.replace(i[1:], i[1:].lower()))
+    result = ' '.join(s for s in new)
+    return result
+'''
+
+def solution(s):
+    answer=''
+    s=s.split(' ')
+    for i in range(len(s)):
+        if not s[i][0].isdecimal():
+            s[i]=s[i][0].upper()+s[i][1:].lower()
+    answer=' '.join(s)
+
+print(solution("for the last week"))
--- programmers/Level2/N개의 최소공배수.py
+++ programmers/Level2/N개의 최소공배수.py
@@ -10,7 +10,8 @@ math.gcd(4,6)   # 최대공약수
 # 다른 풀이
 def solution(arr):
     from math import gcd                            # 최대공약수를 구하는 gcd() import
-    answer = arr[0]                                 # answer을 arr[0]으로 초기화
+    
+    answer = arr[0]                                 # answer을 arr[0]으로 초기화 / 2
 
     for num in arr:                                 # 반복문을 처음부터 끝까지 돈다.
         #1. (arr[0],arr[1])의 최소공배수를 구한 후 answer에 저장
@@ -21,4 +22,4 @@ def solution(arr):
     return answer
  
 
-print(solution([2,6,8,14]	))
+print(solution([2,6,8,14]))
